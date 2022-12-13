54fc4d1e9479cf07e9bdd45d3fdc9dfe4ec97d0a
Andrii Turkin <andrew.turkin@deliveryhero.com>
2022-12-13 15:16:08 +0100

    Set library to be dynamic

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "InputBarAccessoryView",
+            type: .dynamic,
             targets: ["InputBarAccessoryView"])
     ],
     targets: [
